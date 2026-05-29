a5d730180bc1de18e77685e473e551585a84dbde
Celio <celio.boulay@outlook.fr>
2026-05-29 11:22:41 -0400

    git setup

---
+++ ExpanderGraphs/Basic.lean
@@ -0,0 +1,22 @@
+module
+
+import Mathlib.Combinatorics.SimpleGraph.Basic
+
+public section
+
+namespace ExpanderGraphs
+
+universe u
+variable {V : Type u} (G : SimpleGraph V) /- I feel V should be finite -/
+
+section EdgeExpansion
+
+def edgeBoundary : sorry := sorry
+/- maybe use SimpleGraph.ComponentCompl and SimpleGraph.neighborSet -/
+
+
+
+
+
+end EdgeExpansion
+end ExpanderGraphs
