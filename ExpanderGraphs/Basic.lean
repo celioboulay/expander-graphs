c3bdb7b189d190603ead1beac642bd3af32cc06c
Celio <celio.boulay@outlook.fr>
2026-05-29 22:44:53 -0400

    defined edgeBoundary and edgeExpansion

--- ExpanderGraphs/Basic.lean
+++ ExpanderGraphs/Basic.lean
@@ -1,22 +1,27 @@
-module
-
+import Mathlib.Data.Set.Card
+import Mathlib.Data.Real.Archimedean
 import Mathlib.Combinatorics.SimpleGraph.Basic
-
-public section
 
 namespace ExpanderGraphs
 
 universe u
-variable {V : Type u} (G : SimpleGraph V) /- I feel V should be finite -/
-
-section EdgeExpansion
-
-def edgeBoundary : sorry := sorry
-/- maybe use SimpleGraph.ComponentCompl and SimpleGraph.neighborSet -/
+variable {V : Type u} [Fintype V] [DecidableEq V]
+variable (G : SimpleGraph V)
 
 
+/-- The Edge Boundary of a set S, denoted ∂S, is `∂S = E(S, Sᶜ)`.\
+This is the set of edges emanating from the set S to its complement. -/
+def edgeBoundary (S : Set V) : Set (Sym2 V) :=
+  {e ∈ G.edgeSet | ∃ u ∈ S, ∃ v ∈ Sᶜ, e = s(u, v)}
 
 
+/-- The Edge Expansion is defined as: `min{frac{|∂S| / |S|, 0<|S|≤n/2}` -/
+noncomputable def edgeExpansion (G : SimpleGraph V) : ℝ :=
+  let n : ℝ := (Fintype.card V : ℝ)
+  sInf (
+    ((fun S : Set V ↦ (Set.ncard (edgeBoundary G S) : ℝ) / (Set.ncard S : ℝ)) ''
+    {S : Set V | 0 < Set.ncard S ∧ (Set.ncard S : ℝ) ≤ n / 2} : Set ℝ)
+  )
 
-end EdgeExpansion
+
 end ExpanderGraphs
