db623ce3572c0fcaffe43b322916781b739909dd
Celio <celio.boulay@outlook.fr>
2026-05-30 23:15:36 -0400

    expanderFamily

--- ExpanderGraphs/Basic.lean
+++ ExpanderGraphs/Basic.lean
@@ -1,12 +1,13 @@
 import Mathlib.Data.Set.Card
 import Mathlib.Data.Real.Archimedean
-import Mathlib.Combinatorics.SimpleGraph.Basic
+import Mathlib.Data.Setoid.Partition
+import Mathlib.Combinatorics.SimpleGraph.LapMatrix
 
 namespace ExpanderGraphs
 
 universe u
 variable {V : Type u} [Fintype V] [DecidableEq V]
-variable (G : SimpleGraph V)
+variable (G : SimpleGraph V) [DecidableRel G.Adj]
 
 
 /-- The Edge Boundary of a set S, denoted ∂S, is `∂S = E(S, Sᶜ)`.\
@@ -22,6 +23,18 @@ noncomputable def edgeExpansion (G : SimpleGraph V) : ℝ :=
     ((fun S : Set V ↦ (Set.ncard (edgeBoundary G S) : ℝ) / (Set.ncard S : ℝ)) ''
     {S : Set V | 0 < Set.ncard S ∧ (Set.ncard S : ℝ) ≤ n / 2} : Set ℝ)
   )
+
+
+/-- A sequence of k-regular graphs `{Gi}` of size increasing with i
+is a family of Expander Graphs if `∃ ε > 0` s.t. `h(Gi) ≥ ε`, for all i -/
+def expanderFamily (g : ℕ → SimpleGraph V) (ε : ℝ) : Prop :=
+  ε > 0 ∧ ∀ i : ℕ, ε ≤ edgeExpansion (g i)
+
+
+/-- TODO, maybe use adjacency operator instead, cf Tao's notes -/
+def graphLaplacianEigenvalues : Fin ((Fintype.card V)) → ℝ :=
+  let L := SimpleGraph.lapMatrix ℝ G;
+  fun (i : Fin ((Fintype.card V))) => (i : ℝ) -- tmp
 
 
 end ExpanderGraphs
