f41bdde4a8c3ae378ab8f04db00f1d2a2d5006df
Celio <celio.boulay@outlook.fr>
2026-06-02 10:36:04 -0400

    defn adj for multigraphs

--- ExpanderGraphs/Basic.lean
+++ ExpanderGraphs/Basic.lean
@@ -8,6 +8,9 @@ import Mathlib.Data.Set.Card
 import Mathlib.Data.Real.Archimedean
 import Mathlib.Data.Setoid.Partition
 import Mathlib.Combinatorics.Graph.Basic
+import Mathlib.LinearAlgebra.Matrix.Symmetric
+import Mathlib.LinearAlgebra.Matrix.Trace
+import Mathlib.LinearAlgebra.Matrix.Hadamard
 
 /-!
 # Expander Graphs
@@ -21,9 +24,8 @@ TODO: write documentation :)
 
 namespace ExpanderGraphs
 
-universe u
-variable {α : Type u_3} {β : Type u_4}
-variable (G : Graph α β) [Fintype G.vertexSet]
+variable {α β V : Type*} [Fintype V] [DecidableEq α] [DecidableEq β]
+variable (G : Graph α β) [Fintype G.vertexSet] [Fintype G.edgeSet]
 
 /-- The Edge Boundary of a set S, denoted ∂S, is `∂S = E(S, Sᶜ)`.\
 This is the set of edges emanating from the set S to its complement. -/
@@ -31,22 +33,36 @@ def edgeBoundary (S : Set α) : Set β :=
   {e | ∃ u ∈ S, ∃ v ∈ Sᶜ, G.IsLink e u v}
 
 
-/-- The Edge Expansion is defined as: `min{frac{|∂S| / |S|, 0<|S|≤n/2}` -/
-noncomputable def edgeExpansion : ℝ :=
+/-- The Edge Expansion is defined as: `min{frac{|∂S| / |S|, 0<|S|≤n/2}`. -/
+noncomputable def edgeExpansion (G : Graph α β) : ℝ :=
   let n : ℝ := (Set.ncard G.vertexSet : ℝ)
   sInf (
     ((fun S : Set α ↦ (Set.ncard (edgeBoundary G S) : ℝ) / (Set.ncard S : ℝ)) ''
-    {S : Set α | 0 < Set.ncard S ∧ (Set.ncard S : ℝ) ≤ n / 2} : Set ℝ)
+    {S : Set α | S ⊆ G.vertexSet ∧ 0 < Set.ncard S ∧ (Set.ncard S : ℝ) ≤ n / 2} : Set ℝ)
   )
 
 
-/-- A sequence of k-regular graphs `{Gi}` of size increasing with i
-is a family of Expander Graphs if `∃ ε > 0` s.t. `h(Gi) ≥ ε`, for all i -/
+/-- A sequence of k-regular graphs `{g i}` of size increasing with i
+is a family of Expander Graphs if `∃ ε > 0` s.t. `h(Gi) ≥ ε`, for all i. -/
 def expanderFamily (g : ℕ → Graph α β) (ε : ℝ) : Prop :=
   ε > 0 ∧ ∀ i : ℕ, ε ≤ edgeExpansion (g i)
 
 
-/- TODO: Better adjacency matrix cf slides -/
+/-- `A : Matrix α α ℕ` is qualified as the adjacency
+matrix of a multigraph if `A` is symmetric. -/
+structure IsMultiAdjMatrix (A : Matrix α α ℕ) : Prop where
+  symm : A.IsSymm := by aesop
+
+
+/-- `adjMultiplicity u v` is the number of edges connecting `u` and `v`.
+We use the convention that if `u` has a self-loop, this loop counts twice. -/
+noncomputable def adjMultiplicity (G : Graph α β) (u v : α) : ℕ :=
+  {e : G.edgeSet | G.IsLink e u v}.ncard
+
+
+/-- `adjMatrixMulti G` is the adjacency matrix the multigraph `G` -/
+noncomputable def adjMatrixMulti (G : Graph α β) : Matrix α α ℕ :=
+  fun u v => adjMultiplicity G u v
 
 
 end ExpanderGraphs
