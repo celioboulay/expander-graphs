1b87bd08b33b85ed6b5dd2003e39753530d97b4f
Celio <celio.boulay@outlook.fr>
2026-06-01 23:25:51 -0400

    fixed defs for multigraphs

--- ExpanderGraphs/Basic.lean
+++ ExpanderGraphs/Basic.lean
@@ -1,40 +1,52 @@
+/-
+Copyright (c) 2026 Celio Boulay. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Celio Boulay, Dylan Sparrow, Rafael Castro
+-/
+
 import Mathlib.Data.Set.Card
 import Mathlib.Data.Real.Archimedean
 import Mathlib.Data.Setoid.Partition
-import Mathlib.Combinatorics.SimpleGraph.LapMatrix
+import Mathlib.Combinatorics.Graph.Basic
+
+/-!
+# Expander Graphs
+
+TODO: write documentation :)
+
+## References
+* [Irit Dinur. The PCP theorem by gap amplification. ECCC, 2005.][Dinur2005]
+
+-/
 
 namespace ExpanderGraphs
 
 universe u
-variable {V : Type u} [Fintype V] [DecidableEq V]
-variable (G : SimpleGraph V) [DecidableRel G.Adj]
-
+variable {α : Type u_3} {β : Type u_4}
+variable (G : Graph α β) [Fintype G.vertexSet]
 
 /-- The Edge Boundary of a set S, denoted ∂S, is `∂S = E(S, Sᶜ)`.\
 This is the set of edges emanating from the set S to its complement. -/
-def edgeBoundary (S : Set V) : Set (Sym2 V) :=
-  {e ∈ G.edgeSet | ∃ u ∈ S, ∃ v ∈ Sᶜ, e = s(u, v)}
+def edgeBoundary (S : Set α) : Set β :=
+  {e | ∃ u ∈ S, ∃ v ∈ Sᶜ, G.IsLink e u v}
 
 
 /-- The Edge Expansion is defined as: `min{frac{|∂S| / |S|, 0<|S|≤n/2}` -/
-noncomputable def edgeExpansion (G : SimpleGraph V) : ℝ :=
-  let n : ℝ := (Fintype.card V : ℝ)
+noncomputable def edgeExpansion : ℝ :=
+  let n : ℝ := (Set.ncard G.vertexSet : ℝ)
   sInf (
-    ((fun S : Set V ↦ (Set.ncard (edgeBoundary G S) : ℝ) / (Set.ncard S : ℝ)) ''
-    {S : Set V | 0 < Set.ncard S ∧ (Set.ncard S : ℝ) ≤ n / 2} : Set ℝ)
+    ((fun S : Set α ↦ (Set.ncard (edgeBoundary G S) : ℝ) / (Set.ncard S : ℝ)) ''
+    {S : Set α | 0 < Set.ncard S ∧ (Set.ncard S : ℝ) ≤ n / 2} : Set ℝ)
   )
 
 
 /-- A sequence of k-regular graphs `{Gi}` of size increasing with i
 is a family of Expander Graphs if `∃ ε > 0` s.t. `h(Gi) ≥ ε`, for all i -/
-def expanderFamily (g : ℕ → SimpleGraph V) (ε : ℝ) : Prop :=
+def expanderFamily (g : ℕ → Graph α β) (ε : ℝ) : Prop :=
   ε > 0 ∧ ∀ i : ℕ, ε ≤ edgeExpansion (g i)
 
 
-/-- TODO, maybe use adjacency operator instead, cf Tao's notes -/
-def graphLaplacianEigenvalues : Fin ((Fintype.card V)) → ℝ :=
-  let L := SimpleGraph.lapMatrix ℝ G;
-  fun (i : Fin ((Fintype.card V))) => (i : ℝ) -- tmp
+/- TODO: Better adjacency matrix cf slides -/
 
 
 end ExpanderGraphs
