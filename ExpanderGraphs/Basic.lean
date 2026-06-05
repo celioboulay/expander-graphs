3f6e070cd73f1158a4f928d405b299c335556992
Celio <celio.boulay@outlook.fr>
2026-06-05 16:14:44 -0400

    Isoperimetry and blueprint

--- ExpanderGraphs/Basic.lean
+++ ExpanderGraphs/Basic.lean
@@ -3,3 +3,38 @@ Copyright (c) 2026 Celio Boulay. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Celio Boulay, Dylan Sparrow, Rafael Castro
 -/
+
+import Mathlib.Data.Set.Card
+import Mathlib.Data.Real.Basic
+import Mathlib.Combinatorics.SimpleGraph.LapMatrix
+import Mathlib.Algebra.Order.Archimedean.Real.Basic
+
+/-!
+# Expander Graphs
+
+An expander graph is a sparse graph that has strong connectivity properties.
+This file states general definitions and preliminary results.
+
+## References
+* [Spectral and Algebraic Graph Theory, Daniel A. Spielman, 2025][Spielman2025]
+-/
+
+
+variable {V : Type*}
+variable [Fintype V] [DecidableEq V] [Fact (1 < Fintype.card V)]
+variable (G : SimpleGraph V) [DecidableRel G.Adj]
+
+namespace SimpleGraph.Spectral
+
+/-- Eigenvalues of the Laplacian in non-increasing order. -/
+noncomputable def graphLapEigvals : Fin (Fintype.card V) → ℝ :=
+  (SimpleGraph.isHermitian_lapMatrix ℝ G).eigenvalues₀
+
+end SimpleGraph.Spectral
+
+
+namespace ExpanderGraphs
+
+
+
+end ExpanderGraphs
--- ExpanderGraphs/GraphPartitioning.lean
+++ ExpanderGraphs/GraphPartitioning.lean
@@ -4,16 +4,14 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Celio Boulay, Dylan Sparrow, Rafael Castro
 -/
 
-import Mathlib.Data.Set.Card
-import Mathlib.Data.Real.Basic
-import Mathlib.Algebra.Order.Archimedean.Real.Basic
+import ExpanderGraphs.Basic
 import Mathlib.Combinatorics.SimpleGraph.Connectivity.Connected
 
 /-!
-# Expander Graphs
+# Graph Partitioning
 
-An expander graph is a sparse graph that has strong connectivity properties.
-This file states the main general definitions and early results.
+This file defines isoperimetric ratio, and prove results about
+its relation to the second-smallest eigenvalue of the Laplacian.
 
 ## References
 * [Spectral and Algebraic Graph Theory, Daniel A. Spielman, 2025][Spielman2025]
@@ -21,10 +19,10 @@ This file states the main general definitions and early results.
 
 namespace GraphPartitioning
 
-open SimpleGraph
+namespace Isoperimetry
 
-variable {V : Type*} (G : SimpleGraph V) [Finite V]
-
+variable {V : Type*}
+variable [Fintype V] (G : SimpleGraph V)
 /-- The Edge Boundary of a set `S`, denoted `∂S`, is `∂S = E(S, Sᶜ)`.\
 This is the set of edges emanating from the set S to its complement. -/
 def boundary (S : Set V) : Set (Sym2 V) :=
@@ -43,10 +41,36 @@ noncomputable def isoperimetricRatioGraph : ℝ :=
   sInf ((fun S ↦ isoperimetricRatio G S) '' {S : Set V | 0 < S.ncard ∧ (S.ncard : ℝ) ≤ n / 2})
 
 
+
+variable [DecidableEq V] [Fact (1 < Fintype.card V)]
+variable [DecidableRel G.Adj]
+
+open SimpleGraph.Spectral
+
+/-- For every `S ⊂ V`, `θ(S) ≥ λ₂(1 − s)`, where `s = |S|/|V|`. -/
+theorem graphLapEigvals_mul_le_isoperimetricRatio (S : Set V) :
+  let l₂ : ℝ := graphLapEigvals G 1
+  let s  : ℝ := S.ncard / Nat.card V
+  l₂ * (1 - s) ≤ isoperimetricRatio G S := by
+  sorry
+
+
+/-- `θ(G) ≥ λ₂ / 2`. -/
+theorem graphLapEigvals_div_two_le_isoperimetricRatioGraph :
+  let l₂ := (graphLapEigvals G 1)
+  l₂ / 2 ≤ isoperimetricRatioGraph G := by
+  sorry
+
+namespace Connectivity
+
+omit [Fintype V] [DecidableRel G.Adj] [DecidableEq V] in
 /-- The isoperimetricRatio of `G` is strictly positive if and only if `G` is connected. -/
-theorem isoperimetric_positive_iff_connected :
+theorem isoperimetricRatioGraph_pos_iff_connected :
   0 < isoperimetricRatioGraph G ↔ G.Connected := by
   sorry
 
+end Connectivity
+
+end Isoperimetry
 
 end GraphPartitioning
