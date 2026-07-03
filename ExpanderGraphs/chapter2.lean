89600ecc91fc63fbc4c0441506bed0e3c677dba5
Celio <celio.boulay@outlook.fr>
2026-07-03 13:57:43 -0400

    isoperimetry go brr

--- ExpanderGraphs/chapter2.lean
+++ ExpanderGraphs/chapter2.lean
@@ -27,11 +27,14 @@ variable {α β : Type*} [DecidableEq α] [Fintype α] [Fintype β]
 variable {G : WeightedGraph α β} [DecidableRel G.Adj]
 variable (hα : G.vertexSet = Set.univ)
 variable (hβ : G.edgeSet = Set.univ)
-variable {S : Set α} [Fintype S]
+variable {S : Set α} [Fintype S] -- [Fintype Sᶜ]
 
 open Graph
+open NNReal
 
 namespace Isoperimetry
+
+noncomputable section
 
 def edgeBoundary (G : WeightedGraph α β) (S : Set α) : Set β :=
   {e ∈ G.edgeSet | ∃ a ∈ S, ∃ b ∈ Sᶜ, G.IsLink e a b}
@@ -46,5 +49,25 @@ lemma edgeBoundaryComplement (S : Set α) :
   constructor <;> rintro ⟨a, ha, b, hb, h⟩ <;> exact ⟨b, hb, a, ha, h.symm⟩
 
 
+/-- E(A, B) denotes the set of edges with one endpoint in A and one endpoint in B. -/
+def edgeConnection (G : WeightedGraph α β) (A B : Set α) : Set β :=
+  {e ∈ G.edgeSet | ∃ a ∈ A, ∃ b ∈ B, G.IsLink e a b}
+
+
+/-- ∂S = E(S, Sᶜ). -/
+lemma self_connection_eq_boundary (S : Set α) :
+  edgeBoundary G S = edgeConnection G S Sᶜ := rfl
+
+
+/-- We define vol S, the volume of S, to be the sum of the degrees of the vertices in S. -/
+def vol (S : Set α) [Fintype S] : ℝ≥0 := ∑ v : S, G.degree v
+
+
+-- For a vertex set S, we define hG(S) = |E(S, Sᶜ)| / min(vol S , vol Sᶜ). -/
+-- def hG := (edgeBoundary G S).ncard / (min (vol S) (vol Sᶜ))
+
+-- We will only consider connected graphs
+
+end
 
 end Isoperimetry
